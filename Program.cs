645ba1507b07b62f0c158f220f56fd07ebb481bb
Samuelandrade <samu24578@gmail.com>
2024-09-06 13:52:50 -0300

    Reformulação de algumas variáveis e melhor controle de exibilção dos livros disponíveis

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GerenciamentodeBiblioteca
 {
@@ -6,7 +8,8 @@ namespace GerenciamentodeBiblioteca
     {
         static void Main(string []args)
         {
-            Dictionary<string, string> LivrosInfantis = new Dictionary<string, string>()
+            
+            Dictionary<string, string> ColecaodeLivrosInfantis = new Dictionary<string, string>()
             {
                 {"O menino maluquinho", "O menino maluquinho"},
                 {"A galinha dos ovos de ouro", "A galinha dos ovos de ouro"},
@@ -14,45 +17,27 @@ namespace GerenciamentodeBiblioteca
                 {"O pequeno peregrino","O pequeno peregrino"}
             };
 
-            Dictionary<string, string> LivrosMisterio = new Dictionary<string, string>()
+            Dictionary<string, string> ColecaodeLivrosdeMisterio = new Dictionary<string, string>()
             {
                 {"A ilha misteriosa", "A ilha misteriosa"},
                 {"Ninguem vai te ouvir gritar", "Ninguem vai te ouvir gritar"},
                 {"O misterio da casa verde", "O misterio da casa verde"},
                 {"O longo adeus", "O longo adeus"}
             };
-            
-            Livro LivrosParaInfancia = new Livro("Livros Infantis", LivrosInfantis);
-            /*Livro LivrosdeMisterio = new Livro("Livro de Misterio", LivrosMisterio);*/
 
-            LivrosParaInfancia.Emprestar(LivrosInfantis, "O menino maluquinho");
-            /*LivrosdeMisterio.Emprestar(LivrosMisterio, "Ninguem vai te ouvir gritar ");*/
+            Livro LivrosParaInfancia = new Livro("Livros Infantis", ColecaodeLivrosInfantis);
+            Livro LivrosdeMisterio = new Livro("Livro de Misterio", ColecaodeLivrosdeMisterio);
+
+            
+            LivrosParaInfancia.Emprestar(ColecaodeLivrosInfantis, "O menino maluquinho");
+            LivrosdeMisterio.Emprestar(ColecaodeLivrosdeMisterio, "Ninguem vai te ouvir gritar");
             Console.WriteLine("\n");
 
             Console.WriteLine("-----------------------");
             Console.WriteLine("Livros infantis disponíveis : ");
-            foreach (KeyValuePair<string, string> livros in LivrosInfantis)
-            {
-                Console.WriteLine(livros.Value); 
-            }
 
-            /*
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Livros de misterio disponíveis : ");
-            foreach (KeyValuePair<string, string> livros in LivrosMisterio)
-            {
-                Console.WriteLine(livros.Value); 
-            }
-            */
-
-            LivrosParaInfancia.Devolver(LivrosInfantis, "o menino maluquinho");
-
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Livros infantis disponíveis : ");
-            foreach (KeyValuePair<string, string> livros in LivrosInfantis)
-            {
-                Console.WriteLine(livros.Value); 
-            }
+            LivrosParaInfancia.ExibirLivrosDisponiveis(ColecaodeLivrosInfantis);
+            LivrosdeMisterio.ExibirLivrosDisponiveis(ColecaodeLivrosdeMisterio);
         }
     }
 
@@ -74,9 +59,9 @@ namespace GerenciamentodeBiblioteca
             if (livros.ContainsKey(livroAserEmprestado))
             {
                 livros.Remove(livroAserEmprestado);
-                Console.WriteLine($"O livro {livroAserEmprestado} foi emprestado com sucesso \n");   
+                Console.WriteLine($"O livro : {livroAserEmprestado}, foi emprestado com sucesso \n");   
             } else {
-                Console.WriteLine($"O livro {livroAserEmprestado} não existe na bilbioteca ou já está sendo emprestado");
+                Console.WriteLine($"O livro : {livroAserEmprestado}, não existe na bilbioteca ou já está sendo emprestado");
             }
         }
 
@@ -88,6 +73,16 @@ namespace GerenciamentodeBiblioteca
                 Console.WriteLine($"O livro {livroAserDevolvido} foi devolvido com sucesso para a biblioteca \n");   
             } else {
                 Console.WriteLine($"O livro {livroAserDevolvido} que você está tentando devolver está incorreto ou já faz parte da biblioteca ");
+            }
+        }
+    
+        public void ExibirLivrosDisponiveis(Dictionary<string, string> tipodoLivro)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Livros que ainda estão disponíveis : ");
+            foreach (KeyValuePair<string, string> livros in tipodoLivro)
+            {
+                Console.WriteLine(livros.Value); 
             }
         }
     }
